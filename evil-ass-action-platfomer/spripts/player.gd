acebe117e87c39ad1cc875bb23a7e65c38824ee1
66070038 <82157475+yojikunn@users.noreply.github.com>
2026-08-24 01:36:48 +0700

    movement part 2

--- evil-ass-action-platfomer/spripts/player.gd
+++ evil-ass-action-platfomer/spripts/player.gd
@@ -4,25 +4,33 @@ extends CharacterBody2D
 
 
 const SPEED = 300.0
-const JUMP_VELOCITY = -1150.0
+const JUMP_VELOCITY = -1450.0
+const JUMP_DECELERATION = 1500.0
+const FALL_VELOCITY = 500.0
 var is_attack = false
+var is_jump = false
+var is_fall = false
+
 
 func _physics_process(delta: float) -> void:
 	
-	if (velocity.x > 1 or velocity.x < -1) and is_attack == false :
+	if (velocity.x > 1 or velocity.x < -1) and is_attack == false and is_on_floor():
 		animated_sprite_2d.animation = "run"
 	else:
-		if is_attack == false:
+		if is_attack == false and is_fall == false:
 			animated_sprite_2d.animation = "idle"
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
-		if is_attack == false:
-			if velocity.y < -1:
-				animated_sprite_2d.animation = "jump"
-			elif velocity.y > 1:
-				if animated_sprite_2d.animation != "fall":
-					animated_sprite_2d.animation = "fall"
+		if is_attack == false and is_jump == true:
+			animated_sprite_2d.animation = "jump"
+		if is_attack == false and is_fall == true:
+			animated_sprite_2d.animation = "fall"
+	elif Input.is_action_just_pressed("jump"):
+		is_jump = true
+	else:
+		is_fall = false
+
 	
 	if is_attack == true:
 		if is_on_floor() and animated_sprite_2d.animation != "air_attack":
@@ -34,8 +42,16 @@ func _physics_process(delta: float) -> void:
 	# Handle jump.
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
+		is_jump = true
 		jump_sound.play()
 	
+	if is_jump == true:
+		velocity.y = move_toward(velocity.y, 0, JUMP_DECELERATION * delta)
+		
+		if Input.is_action_just_released("jump") or velocity.y >= 0:
+			velocity.y = 0
+			is_jump = false
+			is_fall = true
 	
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
